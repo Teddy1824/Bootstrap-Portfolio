bd818a78d5a56aa070d82196be888954e6f0291b
Teddy1824 <bearnomangola7@gmail.com>
2021-11-20 13:24:57 +0200

    edits

---
+++ JS/myjs.js
@@ -0,0 +1,7 @@
+const menu2 = document.querySelector(".menu2");
+const menuBox = document.querySelector(".menu-box");
+menu2.addEventListener("click", function () {
+  menu2.classList.toggle("active");
+  menuBox.classList.toggle("show");
+  menuBox.classList.toggle("active");
+});
