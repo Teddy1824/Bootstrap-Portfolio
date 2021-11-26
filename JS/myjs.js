857cd4bef3b338e65df9abeddb94a1b466a6ec4f
Bongani <bearnomangola7@gmail.com>
2021-11-26 15:35:38 +0200

    edits

--- JS/myjs.js
+++ JS/myjs.js
@@ -1,8 +1,18 @@
 const menu2 = document.querySelector(".menu2");
 const menuBox = document.querySelector(".menu-box");
-menu2.addEventListener("click", function () {
+// menu2.addEventListener("click", function () {
+//   menu2.classList.toggle("active");
+//   menuBox.classList.toggle("show");
+//   menuBox.classList.toggle("active");
+// });
+
+function toggleMenu() {
   menu2.classList.toggle("active");
   menuBox.classList.toggle("show");
   menuBox.classList.toggle("active");
-});
+}
 
+function scrollToSection(sectionName) {
+  toggleMenu();
+  document.getElementById(sectionName).scrollIntoView();
+}
